99c97c901a68adee423b6a7a127464db41a7a682
carametal <mkatsdevelop@gmail.com>
2025-03-17 22:04:47 +0900

    Neovimを追加

---
+++ nvim/init.lua
@@ -0,0 +1,25 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- 画面表示
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamed"
+vim.opt.encoding = "utf-8"
+vim.opt.termguicolors = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opincsearch = true
+vim.opt.hlsearch = true
+vim.opt.cursorline = true
+vim.opt.wildmenu = true
+vim.opt.showmatch = true
+vim.opt.scrolloff = 5
+
+-- キーバインド
+vim.keymap.set("i", "<C-j>", "<Esc>")
